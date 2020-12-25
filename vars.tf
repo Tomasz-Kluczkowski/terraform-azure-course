04f0095541f1774f909043e94532cc99e479ab81
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2020-12-25 11:07:08 +0000

    Test variables

--- vars.tf
+++ vars.tf
@@ -15,3 +15,20 @@ variable "mylist" {
   type = list(string)
   default = ["tom", "mac"]
 }
+
+// same as in python - set has unique elements but is also always storted
+variable "myset" {
+  type = set(string)
+  default = ["tom", "mac", "bolo"]
+}
+
+// each element of object can be of different type
+variable "myobject" {
+  type = object({})
+}
+
+// tuple is like a list but each element can be of different type
+variable "mytuple" {
+  type = tuple([string, number])
+  default = ["tom", 10]
+}
